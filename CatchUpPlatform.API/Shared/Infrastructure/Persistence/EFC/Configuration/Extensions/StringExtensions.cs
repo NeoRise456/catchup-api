5142bfe5ea4e59d6f2183602239049d588f657c0
neo <neorise456@gmail.com>
2024-10-17 16:22:52 -0500

    feat(orm): add extensions for naming conventions.

--- CatchUpPlatform.API/Shared/Infrastructure/Persistence/EFC/Configuration/Extensions/StringExtensions.cs
+++ CatchUpPlatform.API/Shared/Infrastructure/Persistence/EFC/Configuration/Extensions/StringExtensions.cs
@@ -1,11 +1,25 @@
+using Humanizer;
+
 namespace CatchUpPlatform.API.Shared.Infrastructure.Persistence.EFC.Configuration.Extensions;
 
+/// <summary>
+/// String extensions
+/// </summary>
+/// <remarks>
+/// this class contains the extension methods for string.
+/// </remarks>
 public static class StringExtensions
 {
+    
+    /// <summary>
+    /// Conver String to Snake Case
+    /// </summary>
+    /// <param name="text"> The string to convert </param>
+    /// <returns>the string in Snake Case format </returns>
     public static string ToSnakeCase(this string text)
     {
         return new string(Convert(text.GetEnumerator()).ToArray());
-        
+
         static IEnumerable<char> Convert(CharEnumerator e)
         {
             if (!e.MoveNext()) yield break;
@@ -25,4 +39,14 @@ public static class StringExtensions
         }
     }
     
+    /// <summary>
+    /// Conversion of string to plural
+    /// </summary>
+    /// <param name="text"> The string to convert </param>
+    /// <returns> the string in plural </returns>
+    public static string ToPlural(this string text)
+    {
+        return text.Pluralize(false);
+    }
+
 }
