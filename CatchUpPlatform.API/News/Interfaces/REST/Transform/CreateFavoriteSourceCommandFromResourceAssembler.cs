18607c36d544b9eae81ca90d6b6b25f80f7e6274
neo <neorise456@gmail.com>
2024-10-17 16:43:56 -0500

    feat(favorite-source): add favorite source resource assembers.

---
+++ CatchUpPlatform.API/News/Interfaces/REST/Transform/CreateFavoriteSourceCommandFromResourceAssembler.cs
@@ -0,0 +1,21 @@
+using CatchUpPlatform.API.News.Domain.Model.Commands;
+using CatchUpPlatform.API.News.Interfaces.REST.Resources;
+
+namespace CatchUpPlatform.API.News.Interfaces.REST.Transform;
+
+/// <summary>
+/// Assembler to transform a CreateFavoriteSourceResource to a CreateFavoriteSourceCommand
+/// </summary>
+public static class CreateFavoriteSourceCommandFromResourceAssembler
+{
+    
+    /// <summary>
+    /// Transform a CreateFavoriteSourceResource to a CreateFavoriteSourceCommand
+    /// </summary>
+    /// <param name="resource"> the <see cref="CreateFavoriteSourceResource"/> </param>
+    /// <returns> An instance of <see cref="CreateFavoriteSourceCommand"/> </returns>
+    public static CreateFavoriteSourceCommand ToCommandFromResource(this CreateFavoriteSourceResource resource)
+    {
+        return new CreateFavoriteSourceCommand(resource.NewsApiKey, resource.SourceId);
+    }
+}
